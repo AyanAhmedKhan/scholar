14b2f8549205448a22741f19163c8c29a7c6b642
AyanAhmedKhan <145579926+AyanAhmedKhan@users.noreply.github.com>
2025-12-18 18:13:13 +0530

    feat: Add script to create or update a superuser.

--- backend/scripts/create_superuser.py
+++ backend/scripts/create_superuser.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.db.database import SessionLocal
 from app.models.user import User, UserRole
+from app.models.notice import Notice
 from app.core.security import get_password_hash
 
 def create_superuser():
