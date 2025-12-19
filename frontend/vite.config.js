276b70d42137d1564a8a54ee2c7d6f837c2592aa
AyanAhmedKhan <145579926+AyanAhmedKhan@users.noreply.github.com>
2025-12-20 00:40:36 +0530

    feat: initialize frontend project with Vite, React, and PWA configuration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,35 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'MITS Scholar',
+        short_name: 'MITS Scholar',
+        description: 'Official Scholarship Portal of MITS Gwalior',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
